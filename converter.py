093f7fd4a49c236590e9497388a6c7e12fe0e24b
HeenJoy <106069414+HeenJoy@users.noreply.github.com>
2022-10-22 23:27:10 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,8 +1,16 @@
 from os import path
+import os
 from pydub import AudioSegment
 from xmlgen import xmlgen
+import shutil
 
+try:
+	os.mkdir("out")
+except:
+	pass
+	
 src = input("Enter file name: ")
+destination_path = input("Enter FULL path to main StormWorks folder (Example: C:/Program Files (x86)/Steam/steamapps/common/Stormworks): ") + "/rom/audio/music"
 
 #importing file from location by giving its path
 sound = AudioSegment.from_mp3(src)
@@ -24,10 +32,18 @@ for i in range (1, ranges):
 	
 	if i <= 9:
 		fileout = "out/main_theme_0" + str(i) + ".ogg"
+		fileoutname = "main_theme_0" + str(i) + ".ogg"
 	else:
 		fileout = "out/main_theme_" + str(i) + ".ogg"
+		fileoutname = "main_theme_" + str(i) + ".ogg"
 
 	# Saving file in required location
 	extract.export(fileout, format="ogg")
 
+	os.remove(destination_path + "/" + fileoutname)
+	new_location = shutil.move(fileout, destination_path)
+
 xmlgen(ranges)
+
+os.remove(destination_path + "/main_theme.xml")
+new_location = shutil.move("out/main_theme.xml", destination_path)
