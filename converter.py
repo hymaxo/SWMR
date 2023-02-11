2cf7a76aebdda9a7183dd2b71c69a40a4e08c0c1
HeenJoy <106069414+HeenJoy@users.noreply.github.com>
2023-02-11 11:19:25 +0300

    Bug Fixes

--- converter.py
+++ converter.py
@@ -6,8 +6,7 @@ import shutil
 
 try:
 	os.mkdir("out")
-except:
-	pass
+except:pass
 	
 src = input("Enter file name: ")
 destination_path = input("Enter FULL path to main StormWorks folder (Example: C:/Program Files (x86)/Steam/steamapps/common/Stormworks): ") + "/rom/audio/music"
@@ -19,10 +18,10 @@ EndSec = 0
 ranges = int((sound.duration_seconds // 15) + 2)
 print ("Debug info: \n Count of ogg files: "  + str(ranges))
 
-for i in range (1, ranges):
+for i in range (0, ranges):
 	StrtSec = StrtSec + 15
 	EndSec = EndSec + 15
-	
+
 	# Time to milliseconds conversion
 	StrtTime = StrtSec*1000
 	EndTime = EndSec*1000
@@ -39,11 +38,15 @@ for i in range (1, ranges):
 
 	# Saving file in required location
 	extract.export(fileout, format="ogg")
-
-	os.remove(destination_path + "/" + fileoutname)
+	try:
+		os.remove(destination_path + "/" + fileoutname)
+	except:pass
 	new_location = shutil.move(fileout, destination_path)
 
 xmlgen(ranges)
 
 os.remove(destination_path + "/main_theme.xml")
 new_location = shutil.move("out/main_theme.xml", destination_path)
+
+print("DONE!")
+input("Press ENTER to exit")
